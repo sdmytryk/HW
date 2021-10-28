ed7ce9f8f7d8f44c590a00481c25d21ede4fb2b5
alexandrasyrotenko <55150766+sdmytryk@users.noreply.github.com>
2021-10-28 19:51:37 +0300

    HW#8

--- Прототипи/script.js
+++ Прототипи/script.js
@@ -1,5 +1,5 @@
 class Student {
-  constructor (university, course, fullName) {
+  constructor(university, course, fullName) {
       this.university = university;
       this.course = course;
       this.fullName = fullName;
@@ -7,11 +7,9 @@ class Student {
       this.isStudy = true;
   }
 
-
   getInfo() {
     return `Студент ${this.course}го курсу ${this.university} ${this.fullName}`;
   }
-
 
   get getMarks() {
     if (this.isStudy) {
@@ -21,27 +19,23 @@ class Student {
     }
   }
 
-
   set setMarks(mark) {
     if (this.isStudy) {
       this.marks.push(mark);
-      return this.studentMarks;
+      return this.marks;
     } else {
       return null;
     }
   }
 
-
   getAverageMark() {
     return this.marks.reduce((sum, mark) => sum + mark) / this.marks.length;
   }
-
 
   dismiss() {
     this.isStudy = false;
   }
   
-
   recover() {
     this.isStudy = true;
   }
@@ -63,10 +57,11 @@ console.log(sofia.getMarks);
 
 //Advanced
 class BudgetStudent extends Student {
-  constructor (university, course, fullName) {
+  constructor(university, course, fullName) {
       super(university, course, fullName);
       setInterval(() => this.getScholarship(), 30000)
     }
+    
   getScholarship() {
       if (this.getAverageMark() >= 4 && this.isStudy) {
           console.log('Ви отримали 1400 грн. стипендії');
