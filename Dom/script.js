680ac094ed89a81c558ea5311c49a669bd4637f9
alexandrasyrotenko <55150766+sdmytryk@users.noreply.github.com>
2021-10-28 21:27:33 +0300

    HW#9

--- Dom/script.js
+++ Dom/script.js
@@ -5,10 +5,10 @@ document.body.append(wrapher);
 
 const generateBlocks = () => {
   for (let i = 0; i < 25; i++) {
-    const square = document.createElement('div');
-    square.classList.add('box');
-    square.style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
-    wrapher.append(square);
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2, 8);
+    wrapher.append(box);
   }
 }
 
