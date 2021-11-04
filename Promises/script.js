587032305ec94109afc4b1f87e76c6ea21713616
alexandrasyrotenko <55150766+sdmytryk@users.noreply.github.com>
2021-11-04 20:49:42 +0200

    HW#11

---
+++ Promises/script.js
@@ -0,0 +1,21 @@
+function getRandomChinese(length) {
+    let res = '';
+    let i = 0;
+    
+    return new Promise((resolve) => {
+        while (i < length) {
+            setTimeout(() => {
+                const sign = Date.now() % 100000;
+                res += String.fromCharCode(sign);
+                if (res.length === length) {
+                    resolve(res);
+                }
+            }, i * 50);
+        i++
+        };
+    })
+  }
+
+
+  getRandomChinese(4).then((result) => console.log(result));
+  getRandomChinese(4).then((result) => document.writeln(`${result}`));
